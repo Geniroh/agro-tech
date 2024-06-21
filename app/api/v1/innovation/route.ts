eed7de04a0ff003f1f079026b04bf962348baf8e
Geniroh2 <chibuzor@codematic.io>
2024-06-21 10:05:52 +0100

    image change

--- app/api/v1/innovation/route.ts
+++ app/api/v1/innovation/route.ts
@@ -91,39 +91,6 @@ export async function POST(request: Request) {
   }
 }
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const body = await request.json();
-
-//     const newInnovation = await db.innovation.create({
-//       data: {
-//         productName: body.innovation_name,
-//         yearInvented: body.innovation_year,
-//         country: body.innovation_country,
-//         cost: Number(body.innovation_cost),
-//         productChain: body.innovation_value_chain,
-//         productPhase: body.innovation_phase,
-//         productUse: body.product_usage,
-//         productDescription: body.product_description,
-//         productMedia: body.product_media,
-//         isExample: body.isUsageExample,
-//         productExample: body.instances,
-//         productInstruction: body.instructions,
-//         productInventor: body.inventor,
-//         productSupplier: body.supplier,
-//         productGuidelines: body.hseguidelines,
-//         isGenderFriendly: body.isGenderFriendly,
-//         productGenderDescription: body.gender_description,
-//       },
-//     });
-
-//     return NextResponse.json(newInnovation);
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ error: "Failed to create new innovation" });
-//   }
-// }
-
 export async function GET(req: NextRequest) {
   try {
     const url = new URL(req.url);
