cf26b3f6f0e11eb747ebdbae915a35cd90511fae
Geniroh2 <chibuzor@codematic.io>
2024-07-18 09:55:12 +0100

    refactory 4 added edit feature

--- lib/mail.ts
+++ lib/mail.ts
@@ -36,7 +36,57 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   await sendMail(
     "Confirm your email",
     email,
-    `<p>Click <a href="${confirmLink}">here</a> to confirm email. </p>`
+    // `<p>Click <a href="${confirmLink}">here</a> to confirm email. </p>`
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet">
+        <title>StavMia Reset Password</title>
+    </head>
+    <body style="padding: 40px; 
+        color: #344054; 
+        font-family: Inter; 
+        margin: 0 auto; 
+        max-width: 570px;
+        font-size: 16px;
+        background-color: #fff;">
+        
+        <div >
+            <div class="logo" style="display: flex;
+            align-items: center;
+            gap: 10px;
+            font-size: 24px;
+            font-weight: 600; flex-direction:column;">
+                <img src="https://stavmia-bucket.nyc3.cdn.digitaloceanspaces.com/StavmiaLogo.png" alt="" style="height: 50px; width: 100px">
+                <div></div>
+            </div>
+    
+            <div class="info" style="padding-top: 44px;
+            padding-bottom: 44px;">
+                <p>Dear ${email},</p>
+                <p>Click <a href="${confirmLink}">here</a> to confirm email. </p>
+      
+                <div>
+                    Regards,<br/>Pan African Society of Agricultural Engineering (PASAE)
+                </div>
+            </div>
+            
+    
+        </div>
+    
+        <script>
+            // JavaScript code to set the current year
+            const currentYear = new Date().getFullYear();
+            document.getElementById('currentYear').innerText = currentYear;
+          </script>
+        
+        
+    </body>
+    </html>`
   );
 };
 
@@ -46,7 +96,56 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   await sendMail(
     "Reset your password",
     email,
-    `<p>Click <a href="${resetLink}">here</a> to reset password. </p>`
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet">
+        <title>StavMia Reset Password</title>
+    </head>
+    <body style="padding: 40px; 
+        color: #344054; 
+        font-family: Inter; 
+        margin: 0 auto; 
+        max-width: 570px;
+        font-size: 16px;
+        background-color: #fff;">
+        
+        <div >
+            <div class="logo" style="display: flex;
+            align-items: center;
+            gap: 10px;
+            font-size: 24px;
+            font-weight: 600; flex-direction:column;">
+                <img src="https://stavmia-bucket.nyc3.cdn.digitaloceanspaces.com/StavmiaLogo.png" alt="" style="height: 50px; width: 100px">
+                <div></div>
+            </div>
+    
+            <div class="info" style="padding-top: 44px;
+            padding-bottom: 44px;">
+                <p>Dear ${email},</p>
+                <p>Click <a href="${resetLink}">here</a> to reset password. </p>
+      
+                <div>
+                    Regards,<br/>Pan African Society of Agricultural Engineering (PASAE)
+                </div>
+            </div>
+            
+    
+        </div>
+    
+        <script>
+            // JavaScript code to set the current year
+            const currentYear = new Date().getFullYear();
+            document.getElementById('currentYear').innerText = currentYear;
+          </script>
+        
+        
+    </body>
+    </html>`
   );
 };
 
@@ -84,11 +183,12 @@ export const sendUploadRequest = async (email: string, name: string) => {
 
         <div class="info" style="padding-top: 44px;
         padding-bottom: 44px;">
-            <p>Hi ${name},</p>
-            <p>We’re glad to have you onboard! and uploading your innovation. Our staff are currently reviewing your innovation and would be approved in 2-3 working days.</p>
-            <p>If there’s anything you need, we’ll be here every step of the way.</p>
+            <p>Dear ${name},</p>
+            <p>Your innovation has been successfully uploaded to STAVMiA. Thank you for your contribution. Check your profile in 10 days to see if your upload is approved.</p>
+            <p>If you would like to raise any concerns about your upload in the future, contact the admin..</p>
+  
             <div>
-                Thanks, <br/> StavMia
+                Regards,<br/>Pan African Society of Agricultural Engineering (PASAE)
             </div>
         </div>
         
